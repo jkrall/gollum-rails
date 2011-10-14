d7d340ad556dd6b20eae75d53c2cf71f01794b9c
Joshua Krall <joshuakrall@pobox.com>
2011-10-14 01:10:45 -0500

    Initial rails 3.1.1 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GollumRails::Application.config.secret_token = 'db27ff950aaac9500a5d4a7b77e7f3e5fccaf42956b852fcb50068fe14ba093422a05aed97d12c5a0223acdcf7ffb35cd7ed72060b35d82f5908e8aab082f90b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GollumRails::Application.config.session_store :cookie_store, :key => '_gollum-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GollumRails::Application.config.session_store :active_record_store
